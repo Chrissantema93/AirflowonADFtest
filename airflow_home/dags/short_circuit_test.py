08caff0497e5cb7ee447394aeb322181ff6c31f4
Chris Santema <codesantema@hotmail.com>
2023-09-13 20:45:48 +0000

    test

--- airflow_home/dags/short_circuit_test.py
+++ airflow_home/dags/short_circuit_test.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from datetime import datetime
 import os
 import time
+from airflow_home.include import test
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.python import ShortCircuitOperator
@@ -12,7 +13,7 @@ default_args = {"owner": "test", "retries": 1}
 
 with DAG(
     default_args=default_args,
-    dag_id="short_circuit_test1",
+    dag_id="short_circuit_test2",
     start_date=datetime(2023, 1, 1),
     schedule_interval=None,
     catchup=False,
@@ -30,9 +31,9 @@ with DAG(
         task_id="python_sleep_task", python_callable=lambda: time.sleep(10)
     )
 
-    # python_test_task = PythonOperator(
-    #     task_id="python_test_task", python_callable=test.test_py
-    # )
+    python_test_task = PythonOperator(
+        task_id="python_test_task", python_callable=test.test_py
+    )
     dummy_task_3_run_in_env = ShortCircuitOperator(
         task_id="dummy_task_3_run_in_env",
         doc="ShortCircuit to make sure dummy_task_3 only runs in Development, QA",
@@ -48,6 +49,6 @@ with DAG(
 
 dummy_task_1 >> [dummy_task_2, dummy_task_3_run_in_env]
 dummy_task_2 >> [python_sleep_task]
-# python_sleep_task >> [python_test_task] 
+python_sleep_task >> [python_test_task] 
 dummy_task_3_run_in_env >> [dummy_task_3]
 dummy_task_3 >> [dummy_task_4]
