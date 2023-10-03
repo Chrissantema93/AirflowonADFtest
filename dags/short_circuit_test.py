594705c763db2b5e39a97feb8d8c6d5973a6fb22
Chris Santema <chris.santema@devoteam.com>
2023-10-03 20:12:55 +0200

    removed unused import

--- dags/short_circuit_test.py
+++ dags/short_circuit_test.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from datetime import datetime
 import os
 import time
-from airflow_home.include import test
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.python import ShortCircuitOperator
